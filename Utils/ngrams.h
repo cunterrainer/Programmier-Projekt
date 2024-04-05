01497887eddf794ddad919fc84efaae9e345b8dd
pyvyx <vyx3327@web.de>
2024-04-05 14:28:50 +0200

    Added n-gram struct

---
+++ Utils/ngrams.h
@@ -0,0 +1,17 @@
+
+typedef struct
+{
+    char character;
+    double probability;
+} Next;
+
+
+/*
+int main()
+{
+    Next letters[256];
+
+    letters['a'].character = 'b';
+    letters['a'].probability = 0.9;
+}
+*/
