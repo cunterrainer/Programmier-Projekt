e0eacfdefb0606533bfb5fe2c9064767b77c2c00
Marcho178 <89090122+Marcho178@users.noreply.github.com>
2024-04-23 12:34:54 +0200

    Update main.c

--- generator/main.c
+++ generator/main.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
+#include <stdbool.h>
 #include <stdlib.h>
+#include <time.h>
 
 typedef struct Next {
     char character;
@@ -7,65 +9,201 @@ typedef struct Next {
 } Next;
 
 const int arraySize = 128;
-int ngramArray[128];
+double ngramArray[128];
 int ngram = 2;
+Next data[256];
+double dataArray[256][256];
+char text[100];
+int textLength = 10;
+double requiredPercentage = 0.0564;
+int textGenMode = 1; //0 = highest prob., 1 = Random value over percentage
 
 void writeInFile();
+void readFile();
 void createNgramme();
-void getData();
+void randomNgram();
+void readFileRandom();
+void getFirstLetterByHighestProbability(int letters[2]);
+void getNextLetterByHighestProbability(int letters[2]);
+void generateText();
+void getNextLetterByPercentageProbability(int letters[2]);
+
 
 int main() {
 //    createNgramme();
-    getData();
+//    readFile();
+//    randomNgram();
+    readFileRandom();
+
+    generateText();
+    printf("%s", text);
 
     return 0;
 }
 
-void getData() {
+/*todo:
+ * - für mehr als ein Buchstabe Funktionen anpassen (variabel anpassbar)
+ * */
+
+
+void readFileRandom() {
     FILE *fptr;
+    char line[13];
+    char doubleString[8];
 
-// Open file in read mode
-    fptr = fopen("../ngramme.csv", "r");
+    // Open a file in writing mode
+    fptr = fopen("../ngrammeRandom.csv", "r");
 
-    if (fptr == NULL) printf("File not found");
+    while(fgets(line, 100, fptr)) {
+        int firstLetter = line[0];
+        int secondLetter = line[1];
 
-    char myString[100];
-    Next data[256];
+        for (int i = 3; i < 12; i++) {
+            doubleString[i-3] = line[i];
+        }
 
-    while(fgets(myString, 100, fptr)) {
-        printf("%s", myString);
+        double probability = 0.0;
+        sscanf(doubleString, "%lf", &probability);
 
-        char character = myString[1];
-        double probability = 0.56; //todo: string to double
-        
-        data[myString[0]].character = character;
-        data[myString[0]].probability = probability;
+        dataArray[firstLetter][secondLetter] = probability;
     }
 
-// Print the file content
-    printf("%s", myString);
-
-// Close the file
+    // Close the file
     fclose(fptr);
+}
 
-//    return data;
+void readFile() {
+    FILE *file = fopen("../ngrammeRandom.csv", "r");
+    char doubleString[15] = "";
+    char character;
+    int c;
+    int counterData = 0;
+
+    if (file == NULL) printf("File not found"); //could not open file
+
+    bool isProbability = false;
+    bool isCharacter = true;
+    int i = 0;
+
+    while ((c = fgetc(file)) != EOF) {
+
+        if (isProbability) {
+
+            if (c == '\n') {
+                isProbability = false;
+                double probability = 0.0;
+                sscanf(doubleString, "%lf", &probability);
+
+                data[counterData].character = character;
+                data[counterData++].probability = probability;
+            } else {
+                doubleString[i++] = (char) c;
+            }
+
+        }
+
+        if ((char) c == ',') {
+            isProbability = true;
+            isCharacter = false;
+            i = 0;
+        }
+
+        if ((char) c == '\n') {
+            isCharacter = true;
+        }
+
+        if (isCharacter) {
+            character = (char) c;
+        }
+
+    }
+
+    printf("Finished reading");
+}
+
+void generateText() {
+    int letters[2];
+    getFirstLetterByHighestProbability(letters);
+
+    text[0] = (char) letters[0];
+    text[1] = (char) letters[1];
+    letters[0] = letters[1];
+
+    for (int i = 2; i < textLength; i++) {
+        if (textGenMode == 0) {
+            getNextLetterByHighestProbability(letters);
+        } else if (textGenMode == 1) {
+            getNextLetterByPercentageProbability(letters);
+        }
+
+        text[i] = (char) letters[1];
+        letters[0] = letters[1];
+    }
+
+}
+
+void getFirstLetterByHighestProbability(int letters[2]) {
+    //get highest probability
+    double max = 0.0;
+    for (int i = 65; i < 91; i++) { // 65 - A, 91 - Z, 122 - z
+        for (int j = 65; j < 91; j++) {
+            if (dataArray[i][j] > max) {
+                max = dataArray[i][j];
+                letters[0] = i;
+                letters[1] = j;
+            }
+        }
+    }
+
+}
+
+void getNextLetterByHighestProbability(int letters[2]) {
+    //get highest probability
+    double max = 0.0;
+    int i = letters[0];
+    for (int j = 65; j < 122; j++) {
+        if (dataArray[i][j] > max) {
+            max = dataArray[i][j];
+            letters[1] = j;
+        }
+    }
+
+}
+
+void getNextLetterByPercentageProbability(int letters[2]) {
+    double probabilities[256] = {0};
+    int i = letters[0];
+    int counter = 0;
+    for (int j = 65; j < 122; j++) {
+        if (dataArray[i][j] > requiredPercentage) {
+            probabilities[counter++] = j;
+        }
+    }
+
+    //random Buchstabe aus array zuweisen
+    int randomNum = 257;
+    int loopCounter = 0;
+    do {
+        srand(time(NULL));
+        randomNum = rand() % 257; //todo: optimieren
+        if (loopCounter++ > 100) {
+            randomNum = 0;
+            break;
+        }
+    } while (probabilities[randomNum] == 0);
+
+    letters[1] = probabilities[randomNum];
 }
 
 
-
-
-
-
-
-
-// Erzeugen von Testdaten
+// ------------------------------------------------- Erzeugen von Testdaten -------------------------------------------------
 
 void createNgramme() {
     FILE *datei;
     int c;
 
     // Datei öffnen
-    datei = fopen("../generator/file.txt", "r");
+    datei = fopen("../file.txt", "r");
 
     if (datei == NULL) {
         printf("Error: File not found");
@@ -73,7 +211,7 @@ void createNgramme() {
         // Zeichenweise lesen und ausgeben
         while ((c = fgetc(datei)) != EOF) {
             putchar(c);
-            ngramArray[c] += 1;
+            ngramArray[c] += 1.0;
         }
 
         // Datei schließen
@@ -91,10 +229,28 @@ void writeInFile() {
 
     // Write some text to the file
     for (int i = 0; i < arraySize; i++) {
-        fprintf(fptr, "%c,%d \n", i, ngramArray[i]);
+        fprintf(fptr, "%c,%lf \n", i, ngramArray[i]);
     }
 
     // Close the file
     fclose(fptr);
 }
 
+//create random Ngram
+void randomNgram() {
+    FILE *fptr;
+
+    // Open a file in writing mode
+    fptr = fopen("../ngrammeRandom.csv", "w");
+
+    for (int i = 65; i < 123; i++) {
+        for (int j = 65; j < 123; j++) {
+            double randomNum = (double)rand() / (double)RAND_MAX * 60000;
+            double value = randomNum / 100000;
+            fprintf(fptr, "%c%c,%lf \n", i, j, value);
+        }
+    }
+
+    // Close the file
+    fclose(fptr);
+}
