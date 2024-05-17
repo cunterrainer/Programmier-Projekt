2e0036c88fea6b30dcc97b75aefaa21acb1248a2
Elnaazi <164521850+Elnaazi@users.noreply.github.com>
2024-05-17 11:46:38 +0200

    occurences of trigram

--- n-gram-generator/ngrams2.c
+++ n-gram-generator/ngrams2.c
@@ -7,22 +7,26 @@
 typedef struct {
     char pair[3]; // Pair of characters plus null terminator
     int occurrence;
+    int following[N];
 } Next;
 
 // Counting occurrences of character pairs and returning the occurrence value
 void countOccurence(char str[], Next pairs[]) {
-    for (int i = 0; str[i] != '\0' && str[i + 1] != '\0'; i++) { // Ensure there is a pair
+    for (int i = 0; str[i] != '\0' && str[i + 1] != '\0'; i++) { // making sure there is a pair
         if (str[i]==' '||str[i + 1] == ' ') {
             continue;
-        } // Skip pairs where the second character is a space
+        } // Skip pairs where one of the characters is a blank space
 
         char current_pair[3] = {str[i], str[i + 1], '\0'}; // Create a pair of characters
+        char following_char=str[i+2];//the following after i+1 should be unequal to 0
         
         int found = 0;
+        int pair_index=0;
         for (int j = 0; j < N; j++) {
             if (strcmp(current_pair, pairs[j].pair) == 0) {
                 pairs[j].occurrence++; // Increment the occurrence of the character pair
                 found = 1;
+                pair_index=j;
                 break;
             }
         }
@@ -37,8 +41,13 @@ void countOccurence(char str[], Next pairs[]) {
                 }
             }
         }
+        //TRTING to incremnt the occurence of the following
+        if(following_char!='\0'){
+            pairs[pair_index].following[following_char]++;       
+        }
     }
-}
+}//broken from here
+
 
 int main() {
     char str[] = "Hi this a test string with strings";
@@ -52,7 +61,12 @@ int main() {
         if (pairs[i].occurrence > 0) {
             printf("Occurrences of %s = %d\n", pairs[i].pair, pairs[i].occurrence);
         }
+        for (int j=0;j<N;j++){
+            
+            if (pairs[i].following[j]>0){
+                printf("Occurences of %c after %s= %d\n",j,pairs[i].pair,pairs[i].following[j]);
+            }
+        }
     }
-    
     return 0;
 }
