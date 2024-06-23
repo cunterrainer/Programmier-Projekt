19e766cd935e2eb3500c2960d2c9f58d94a61f51
Marc Rödl <marcroedl34@gmail.com>
2024-06-23 13:00:08 +0200

    add old functions

--- generator/oldFunctions.c
+++ generator/oldFunctions.c
@@ -1,3 +1,108 @@
+void freeMalloc() {
+    // Free the allocated memory to avoid memory leaks
+    for (int i = 0; i < outer_size; i++) {
+        if (arrayMalloc[i] == NULL) {
+            continue;
+        }
+        for (int j = 0; j < middle_size; j++) {
+            if (arrayMalloc[i][j] == NULL) {
+                continue;
+            }
+            free(arrayMalloc[i][j]);
+        }
+        free(arrayMalloc[i]);
+    }
+    free(arrayMalloc);
+}
+
+int readFileWithMalloc() {
+    FILE* fptr;
+    // Open a file in writing mode
+    fptr = fopen("../ngrammeRandom.csv", "r");
+    char line[13];
+    int isEOF = 0;
+
+    // Define the size of the outer array
+    arrayMalloc = (double***)malloc(outer_size * sizeof(double**));
+
+    int trash = 256; //todo: fgets ändert diesen wert auf 10, WARUM?!??!?! Wird er entfernt, wird die Variable darüber auf 10 gesetzt
+
+    int oldFirstLetter = -1;
+    int oldSecondLetter = -1;
+
+    fgets(line, 100, fptr);
+    int firstLetter = line[0];
+    int secondLetter = line[1];
+    int thirdLetter = line[2];
+
+    char doubleString[8];
+    for (int i = 4; i < 12; i++) {
+        doubleString[i-4] = line[i];
+    }
+
+    double probability = 0.0;
+    probability = atof(doubleString);
+
+    for (int i = 0; i < outer_size; i++) {
+        if (isEOF == 1) {
+            arrayMalloc[i] = NULL;
+            continue;
+        }
+
+        if (i != firstLetter) {
+            arrayMalloc[i] = NULL;
+            continue;
+        }
+
+        if (oldFirstLetter != firstLetter) {
+            arrayMalloc[i] = (double**) malloc(middle_size * sizeof(double *));
+            oldFirstLetter = firstLetter;
+        }
+
+        for (int j = 0; j < middle_size; j++) {
+            if (isEOF == 1) {
+                arrayMalloc[i][j] = NULL;
+                continue;
+            }
+
+            if (j != secondLetter) {
+                arrayMalloc[i][j] = NULL;
+                continue;
+            }
+
+            if (oldSecondLetter != secondLetter) {
+                arrayMalloc[i][j] = (double*) malloc(inner_size * sizeof(double));
+                oldSecondLetter = secondLetter;
+            }
+
+            for (int k = 0; k < inner_size; k++) {
+                if (k != thirdLetter) continue;
+
+                arrayMalloc[i][j][k] = probability;  // Assign a simple value based on index
+                if (!fgets(line, 100, fptr)) {
+                    isEOF = 1;
+                    break;
+                }
+
+                firstLetter = line[0];
+                secondLetter = line[1];
+                thirdLetter = line[2];
+
+                for (int m = 4; m < 12; m++) {
+                    doubleString[m-4] = line[m];
+                }
+
+                probability = atof(doubleString);
+            }
+        }
+    }
+
+    // Close the file
+    fclose(fptr);
+
+    return 0;
+}
+
 
 void readFileNew() {
 
@@ -249,4 +354,42 @@ void createNgramme() {
         writeInFile();
     }
 
+}
+
+
+// ------------------------------------------------- Erzeugen von Testdaten -------------------------------------------------
+void writeInFile() {
+    FILE *fptr;
+
+    // Open a file in writing mode
+    fptr = fopen("../ngramme.csv", "w");
+
+    // Write some text to the file
+    for (int i = 0; i < arraySize; i++) {
+        fprintf(fptr, "%c,%lf \n", i, ngramArray[i]);
+    }
+
+    // Close the file
+    fclose(fptr);
+}
+
+//create random Ngram
+void randomNgram() {
+    FILE *fptr;
+
+    // Open a file in writing mode
+    fptr = fopen("../ngrammeRandom.csv", "w");
+
+    for (int i = 65; i < 123; i++) {
+        for (int j = 65; j < 123; j++) {
+            for (int k = 65; k < 123; k++) {
+                double randomNum = (double)rand() / (double)RAND_MAX * 60000;
+                double value = randomNum / 100000;
+                fprintf(fptr, "%c%c%c,%lf \n", i, j, k, value);
+            }
+        }
+    }
+
+    // Close the file
+    fclose(fptr);
 }
